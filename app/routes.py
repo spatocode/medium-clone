6271e03ff7f286d8d78ab84bada7e942bdecfe52
Spatocode <impulseofficial069@gmail.com>
2018-09-12 21:11:30 +0100

    login route

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template
+from app.forms import LoginForm
 from app import app
 
 @app.route('/')
-@app.route('/home')
 def index():
     user = {'username': 'Ekene'}
     posts = [
@@ -16,3 +16,9 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
