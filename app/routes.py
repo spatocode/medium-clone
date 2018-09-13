a072ef6f7527960270065c89de9a441d7fe8b2e0
Spatocode <impulseofficial069@gmail.com>
2018-09-13 18:10:43 +0100

    Includes url method

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 from app import app
 
@@ -24,5 +24,5 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect('/')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
