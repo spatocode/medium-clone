ac99be560e873c02f95f8d39b8e7daa22a192893
Spatocode <impulseofficial069@gmail.com>
2018-09-15 18:35:57 +0100

    Added registration view

--- app/routes.py
+++ app/routes.py
@@ -6,18 +6,7 @@ from app.models import User
 
 @app.route('/')
 def index():
-    user = {'username': 'Ekene'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home')
 
 
 @app.route('/login', methods=['GET', 'POST'])
