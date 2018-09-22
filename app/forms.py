7371d0f455c7ff3364e3252fa42cae0d527c643b
Spatocode <impulseofficial069@gmail.com>
2018-09-22 17:43:12 +0100

    initiated posts logic

--- app/forms.py
+++ app/forms.py
@@ -27,6 +27,10 @@ class RegistrationForm(FlaskForm):
         if user is not None:
             raise ValidationError('Please use a different email address.')
 
+class PostForm(FlaskForm):
+    post = TextAreaField('Say something', validators=[DataRequired(), Length(min=1, max=140)])
+    submit = SubmitField('Submit')
+
 class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
